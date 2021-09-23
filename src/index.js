fa3082fd50033967c5f229bbb742f8f61a8107e3
Graham Kaemmer <gkaemmer@users.noreply.github.com>
2021-09-23 15:30:32 -0400

    Add oso values widget

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import WhatIsOso from "./WhatIsOso";
+import OsoValues from "./OsoValues";
 import "./style.css";
 window.process = undefined;
 
 export function run() {
   window.addEventListener("load", () => {
-    const target = document.getElementById("oso-web-demo");
-    ReactDOM.render(<WhatIsOso />, target);
+    const demoTarget = document.getElementById("oso-web-demo");
+    if (demoTarget) ReactDOM.render(<WhatIsOso />, demoTarget);
+
+    const valuesTarget = document.getElementById("oso-values-widget");
+    if (valuesTarget) ReactDOM.render(<OsoValues />, valuesTarget);
   });
 }
